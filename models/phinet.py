bd5c4b54c3f1c059c4b6f0dcc464e54271037ff8
sremedios <sam.remedios@gmail.com>
2018-04-09 14:49:28 -0500

    updated model

--- models/phinet.py
+++ models/phinet.py
@@ -1,6 +1,7 @@
 from keras.engine import Input, Model
 from keras.layers import Conv3D, MaxPooling3D, GlobalAveragePooling3D,\
-                         AveragePooling3D, Dense, Flatten
+                         GlobalMaxPooling3D, AveragePooling3D, Dense, Flatten,\
+                         Conv1D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.core import Reshape, Activation
 from keras.layers.merge import Concatenate, add
@@ -15,42 +16,35 @@ def phinet(n_classes, n_channels=1, learning_rate=1e-3):
 
     inputs = Input(shape=(None,None,None,n_channels))
 
-    x = Conv3D(16, (3,3,3), strides=(2,2,2), padding='same')(inputs)
+    x = Conv3D(8, (3,3,3), strides=(2,2,2), padding='same')(inputs)
     x = MaxPooling3D(pool_size=(3,3,3), strides=(2,2,2), padding='same')(x)
 
-    x = Conv3D(16, (3,3,3), strides=(2,2,2), padding='same')(x)
-    x = BatchNormalization()(x)
-    y = Activation('relu')(x)
-    x = Conv3D(32, (3,3,3), strides=(1,1,1), padding='same')(y)
-    x = BatchNormalization()(y)
-    x = add([x, y])
-    x = Activation('relu')(x)
-
-    for _ in range(10):
-        x = Conv3D(32, (3,3,3), strides=(1,1,1), padding='same')(x)
+    for _ in range(3):
+        x = Conv3D(8, (3,3,3), strides=(1,1,1), padding='same')(x)
         x = BatchNormalization()(x)
         y = Activation('relu')(x)
-        x = Conv3D(32, (3,3,3), strides=(1,1,1), padding='same')(y)
+        x = Conv3D(8, (3,3,3), strides=(1,1,1), padding='same')(y)
         x = BatchNormalization()(x)
         x = add([x, y])
         x = Activation('relu')(x)
 
     # this block will pool a handful of times to get the "big picture" 
-    y = AveragePooling3D(pool_size=(7,7,7), strides=(4,4,4), padding='same')(inputs)
+    y = AveragePooling3D(pool_size=(3,3,3), strides=(2,2,2), padding='same')(inputs)
     y = MaxPooling3D(pool_size=(3,3,3), strides=(2,2,2), padding='same')(y)
+    y = Conv3D(8, (3,3,3), strides=(1,1,1), padding='same')(y)
 
     # this layer will preserve original signal
-    z = Conv3D(16, (3,3,3), strides=(2,2,2), padding='same')(inputs)
-    z = Conv3D(16, (3,3,3), strides=(2,2,2), padding='same')(z) 
-    z = Conv3D(16, (3,3,3), strides=(2,2,2), padding='same')(z)
+    z = Conv3D(8, (3,3,3), strides=(2,2,2), padding='same')(inputs)
+    z = Conv3D(8, (3,3,3), strides=(2,2,2), padding='same')(z)
+    z = Conv3D(8, (3,3,3), strides=(1,1,1), padding='same')(z)
 
     x = Concatenate(axis=4)([x, y, z])
 
     # global avg pooling before FC
     x = GlobalAveragePooling3D()(x)
-    x = Dense(128, activation='relu')(x)
+    x = Dense(n_classes)(x)
 
-    pred = Dense(n_classes, activation='softmax')(x)
+    pred = Activation('softmax')(x)
     
     model = Model(inputs=inputs, outputs=pred)
 
